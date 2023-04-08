f0874f94edf8e8d6eeb5007aad98d50573b59c81
maxsands700 <maxsands700@gmail.com>
2023-04-08 10:21:19 -0400

    PEAD Article

--- R scripts/plot_formatting.R
+++ R scripts/plot_formatting.R
@@ -1,9 +1,7 @@
 library(ggplot2)
-theme_max_bw <- theme_bw(base_size = 16, base_family = "serif") +
-    theme(plot.title = element_text(hjust = .5),
-          plot.subtitle = element_text(hjust = .5),
-          legend.position = c(.1, .8),
-          legend.background = element_rect(fill = RColorBrewer::brewer.pal(9, "Greys")[2],
-                                           color = RColorBrewer::brewer.pal(9, "Greys")[4]),
-          plot.caption = element_text(size = 10, face = "italic",
-                                      color = RColorBrewer::brewer.pal(9, "Greys")[5]))
+theme_max <- theme(
+        plot.title = element_text(hjust = .5),
+        plot.subtitle = element_text(hjust = .5),
+        plot.caption = element_text(size = 10, face = "italic",
+                                    margin = margin(t = 20))
+    )
