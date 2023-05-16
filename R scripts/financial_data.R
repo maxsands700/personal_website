b901a6dea1d8fff8e2d6301e66f9ee4039886a6f
maxsands700 <maxsands700@gmail.com>
2023-05-16 14:39:04 -0400

    PEAD

--- R scripts/financial_data.R
+++ R scripts/financial_data.R
@@ -37,3 +37,39 @@ alpha_query_get_upcoming_earnings_data <- function(ticker){
 yf_get_price_data <- function(ticker, date, end_date){
     return(try(tq_get(ticker, from = date, to = end_date)))
 }
+
+
+# Calculations ----
+calculate_rolling_correlation <- function(data, target_ticker, periods) {
+    data_split <- data %>% split(.$ticker)
+    target_data <- data_split[[target_ticker]]
+    
+    calculate_cor <- function(target_data, other_data, period) {
+        merged_data <- full_join(target_data, other_data, by = "date") %>%
+            select(date, target = price.x, other = price.y) %>%
+            na.omit()
+        
+        result <- rollapply(data = merged_data[, c("target", "other")], 
+                            width = period, 
+                            FUN = function(x) cor(x[, 1], x[, 2]), 
+                            by.column = FALSE, 
+                            align = "right")
+        
+        return(data.frame(date = merged_data$date[(period):nrow(merged_data)], cor = result))
+    }
+    
+    results <- list()
+    for (ticker in names(data_split)) {
+        if (ticker != target_ticker) {
+            for (period in periods) {
+                result <- calculate_cor(target_data, data_split[[ticker]], period)
+                colname <- paste0("cor_", period, "_", target_ticker, "_", ticker)
+                result <- result %>% rename(!!colname := cor)
+                results[[colname]] <- result
+            }
+        }
+    }
+    
+    output <- reduce(results, left_join, by = "date")
+    return(as_tibble(output))
+}
