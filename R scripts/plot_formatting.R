83036f25652dc6f0c4da8a6f6584792283ed2470
maxsands700 <maxsands700@gmail.com>
2023-06-07 17:04:32 -0400

    Minor Changes

--- R scripts/plot_formatting.R
+++ R scripts/plot_formatting.R
@@ -1,7 +1,9 @@
 library(ggplot2)
 theme_max <- theme(
-        plot.title = element_text(hjust = .5),
-        plot.subtitle = element_text(hjust = .5),
+        plot.title = element_text(hjust = .5, size = 20),
+        plot.subtitle = element_text(hjust = .5, size = 16),
         plot.caption = element_text(size = 10, face = "italic",
-                                    margin = margin(t = 20))
+                                    margin = margin(t = 20)),
+        axis.title.x = element_text(margin = margin(t = 10)),
+        text = element_text(family = "AppleGothic")
     )
