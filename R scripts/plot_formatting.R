bfce6e9931036a97e34aaed3e9797f6750e28b31
maxsands700 <maxsands700@gmail.com>
2023-08-07 10:07:08 -0400

    Bull vs. Bear

--- R scripts/plot_formatting.R
+++ R scripts/plot_formatting.R
@@ -1,9 +1,10 @@
 library(ggplot2)
-theme_max <- theme(
-        plot.title = element_text(hjust = .5, size = 20),
-        plot.subtitle = element_text(hjust = .5, size = 16),
-        plot.caption = element_text(size = 10, face = "italic",
-                                    margin = margin(t = 20)),
-        axis.title.x = element_text(margin = margin(t = 10)),
-        text = element_text(family = "AppleGothic")
+theme_max <- function(){
+    theme(
+        panel.grid.minor = element_blank(),
+        axis.title = element_text(face = "italic"),
+        plot.subtitle = element_text(face = "italic"),
+        plot.caption = element_text(face = "italic", size = 10),
+        legend.position = "top"
     )
+}
